562c988c394873d49ed7533d99bb4454f3ac1909
vladius25 <vkorol2509@icloud.com>
2019-05-07 02:01:09 +0500

    Fix lag

---
+++ build.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+
+if [ ! -d "venv" ]; then
+  echo "Can't find venv. It will be create"
+  ./init_venv.sh
+fi
+. venv/bin/activate
+make clean
+make html
+
+# We need to move css includes before js to prevent loading lag
+# No idea how to fix this
+for file in build/html/*html; do
+    links=$(sed -n '/.*rel=\"stylesheet\"/p' $file)
+    sed -i "/.*rel=\"stylesheet\"/d" $file
+    awk -v line="$links" "1;/.*<title>/{ print line}" $file > $file.tmp
+    mv $file.tmp $file
+    echo $file updated 
+done
+
