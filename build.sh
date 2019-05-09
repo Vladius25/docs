906c377889d44941eaf96f2623e5eeca3f1c27c2
vladius25 <vkorol2509@icloud.com>
2019-05-10 00:28:01 +0500

    Add error handler

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 # !/bin/bash
 
+die () {
+  echo $1
+  exit 1 
+}
+
 if [ ! -d "venv" ]; then
   echo "Can't find venv. It will be created"
-  ./init_venv.sh
+  ./init_venv.sh || die "Can't create venv"
 fi
 . venv/bin/activate
 make clean
-make html
+make html || die "Can't build html"
 echo ""
 
 # We need to move css includes before js to prevent loading lag
@@ -14,8 +19,7 @@ echo ""
 for file in build/html/*html; do
     links=$(sed -n '/.*rel=\"stylesheet\"/p' $file)
     sed -i "/.*rel=\"stylesheet\"/d" $file
-    awk -v line="$links" "1;/.*<title>/{ print line}" $file > $file.tmp
+    awk -v line="$links" "1;/.*<title>/{ print line}" $file > $file.tmp || die "Can't update $file"
     mv $file.tmp $file
     echo $file updated 
 done
-
